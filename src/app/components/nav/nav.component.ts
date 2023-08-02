704bfea511a6b81ddb3d21828d50e4992c9500f3
jose alejandro <josealejandroortegamora@gmail.com>
2023-08-02 08:28:42 -0500

    se agregan iconos al nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
+import { faHouse, faUsers, faHospitalUser, faLaptopMedical, faKey, faFileLines, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
@@ -8,6 +9,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+
+//iconos
+inicio = faHouse;
+usuarios = faUsers;
+pacientes = faHospitalUser;
+consultas = faLaptopMedical;
+roles = faKey;
+reportes = faFileLines;
+salir = faRightFromBracket;
+
+isHovered = false;
+
+onIconHover(isHovered: boolean): void {
+  this.isHovered = isHovered;
+}
+
   isMenuOpen: boolean = false;
 
   constructor(private router: Router, private loginService: LoginService) {}
